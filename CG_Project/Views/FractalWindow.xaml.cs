97cffaa620b9a3ec6af031921d60470cea8e8d3e
Roman Alberda <ramzesball@gmail.com>
2021-11-07 18:52:28 +0200

    Added dragon curve geometrical

--- CG_Project/Views/FractalWindow.xaml.cs
+++ CG_Project/Views/FractalWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Shapes;
 using CG_Project.Helpers;
@@ -20,7 +21,7 @@ namespace CG_Project
         public FractalWindow()
         {
             InitializeComponent();
-            //kochSnowflake = new KochSnowflake(fractalCanvas);
+            kochSnowflake = new KochSnowflake(fractalCanvas);
 
             //for (int i = 0; i < 10000; ++i)
             //{
@@ -51,7 +52,7 @@ namespace CG_Project
 
         private void BuildFractal_OnClick(object sender, RoutedEventArgs e)
         {
-            //fractalCanvas.Children.Clear();
+            fractalCanvas.Children.Clear();
             var checkedValue = radioButtonPannel.Children.OfType<RadioButton>()
                 .FirstOrDefault(r => r.IsChecked.HasValue && r.IsChecked.Value);
             switch (fractalTypes?.SelectedIndex)
@@ -63,6 +64,19 @@ namespace CG_Project
                     }
                     else if (checkedValue.Content.ToString() == FractalNames.DragonCurve)
                     {
+
+                        // Find the first control points.
+                        float dx = (float)(Math.Min(
+                            fractalCanvas.Width / 1.5f,
+                            fractalCanvas.Height) / 3f);
+
+                        // Try to center it a bit.
+                        float x0 = (float)((fractalCanvas.Width - dx * 1.5f) / 2f + dx / 3f);
+                        float y0 = (float)((fractalCanvas.Height - dx) / 2f + dx / 3f);
+
+                        // Recursively draw the lines.
+                        int level = Convert.ToInt32(numberOfIterations.Text);
+                        DrawDragonLine(level, Direction.Right, x0, y0, 2 * dx, 0);
 
                     }
                     break;
@@ -213,6 +227,66 @@ namespace CG_Project
             }
             x = nextX;
             y = nextY;
+        }
+
+        // The direction the curve should turn next.
+        private enum Direction
+        {
+            Left,
+            Right
+        }
+
+        private void DrawDragonLine( int level, Direction turn_towards, float x1, float y1, float dx, float dy)
+        {
+            if (level <= 0)
+            {
+
+                var line = new Line();
+                line.Stroke = Brushes.Black;
+
+                line.X1 = x1;
+                line.Y1 = y1;
+
+                line.X2 = x1 + dx;
+                line.Y2 = y1 + dy;
+
+                line.StrokeThickness = 2;
+                fractalCanvas.Children.Add(line);
+
+#if DRAW_POINTS
+                gr.DrawEllipse(Pens.Blue, x1 - 2, y1 - 2, 4, 4);
+                gr.DrawEllipse(Pens.Blue, x1 + dx - 2, y1 + dy - 2, 4, 4);
+#endif
+            }
+            else
+            {
+#if DRAW_LEVEL_MINUS_1
+                if (level == 1)
+                {
+                    gr.DrawLine(Pens.Silver, x1, y1, x1 + dx, y1 + dy);
+                }
+#endif
+                float nx = (float)(dx / 2);
+                float ny = (float)(dy / 2);
+                float dx2 = -ny + nx;
+                float dy2 = nx + ny;
+                if (turn_towards == Direction.Right)
+                {
+                    // Turn to the right.
+                    DrawDragonLine(level - 1, Direction.Right, x1, y1, dx2, dy2);
+                    float x2 = x1 + dx2;
+                    float y2 = y1 + dy2;
+                    DrawDragonLine(level - 1, Direction.Left, x2, y2, dy2, -dx2);
+                }
+                else
+                {
+                    // Turn to the left.
+                    DrawDragonLine(level - 1, Direction.Right, x1, y1, dy2, -dx2);
+                    float x2 = x1 + dy2;
+                    float y2 = y1 - dx2;
+                    DrawDragonLine(level - 1, Direction.Left, x2, y2, dx2, dy2);
+                }
+            }
         }
 
 
