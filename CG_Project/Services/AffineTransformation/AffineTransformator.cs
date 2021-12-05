f2884dec744c0a54dc554770195673f99ef2d57d
RomanAlberdaSoftserveInc <ralbe@softserveinc.com>
2021-12-05 02:42:33 +0200

    Added affine transformation

---
+++ CG_Project/Services/AffineTransformation/AffineTransformator.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Windows;
+
+namespace CG_Project.Services.AffineTransformation
+{
+    class AffineTransformator
+    {
+        public double[,] Transform(double[,] inputParallelogram, double coeffA, double coeffB)
+        {
+            var radians = Math.Atan(coeffA);
+            //var radians1 = -Math.Atan(70.1);
+            //double degrees = (180 / Math.PI) * radians;
+
+            var simplfyToB = ReturnSimplifierToB(coeffB);
+            var turnedParallelogramAtCenter = Multiply(simplfyToB, ReturnTurnTransformationMatrix(-radians));
+            var mirrorMatrix = Multiply(turnedParallelogramAtCenter, ReturnMirrorTransformationMatrix());
+
+            var backTransformation = Multiply(mirrorMatrix, ReturnBackTurnTransformationMatrix(radians));   
+            var backTransformationToPlusB = Multiply(backTransformation, ReturnSimplifierToPlusB(coeffB));
+
+            return Multiply(inputParallelogram, backTransformationToPlusB);
+        }
+
+        public double[,] ReturnSimplifierToB(double b)
+        {
+            double[,] matr = {{ 1,  0, 0 },
+                              { 0,  1, 0 },
+                              { 0, -b, 1 }};
+            return matr;
+        }
+
+        public double[,] ReturnTurnTransformationMatrix(double a)
+        {
+            double[,] matr = {{  Math.Cos(a), -Math.Sin(a), 0 },
+                              {  Math.Sin(a),  Math.Cos(a), 0 },
+                              {            0,            0, 1 }};
+            return matr;
+        }
+
+        public double[,] ReturnMirrorTransformationMatrix()
+        {
+            double[,] matr = {{  0,   1,  0 },
+                              {  1,   0,  0 },
+                              {  0,   0,  1 }};
+            return matr;
+        }
+
+        public double[,] ReturnBackTurnTransformationMatrix(double a)
+        {
+            double[,] matr = {{   Math.Cos(a),  Math.Sin(a), 0 },
+                              {  -Math.Sin(a),  Math.Cos(a), 0 },
+                              {             0,            0, 1 }};
+            return matr;
+        }
+
+        public double[,] ReturnSimplifierToPlusB(double b)
+        {
+            double[,] matr = {{ 1, 0, 0 },
+                              { 0, 1, 0 },
+                              { 0, b, 1 }};
+            return matr;
+        }
+
+        public static double[,] Multiply(double[,] matrix1, double[,] matrix2)
+        {
+            var matrix1Rows = matrix1.GetLength(0);
+            var matrix1Cols = matrix1.GetLength(1);
+            var matrix2Rows = matrix2.GetLength(0);
+            var matrix2Cols = matrix2.GetLength(1);
+
+            if (matrix1Cols != matrix2Rows)
+                throw new InvalidOperationException
+                  ("Product is undefined. n columns of first matrix must equal to n rows of second matrix");
+
+            double[,] product = new double[matrix1Rows, matrix2Cols];
+
+            // looping through matrix 1 rows  
+            for (int matrix1_row = 0; matrix1_row < matrix1Rows; matrix1_row++)
+            {
+                // for each matrix 1 row, loop through matrix 2 columns  
+                for (int matrix2_col = 0; matrix2_col < matrix2Cols; matrix2_col++)
+                {
+                    // loop through matrix 1 columns to calculate the dot product  
+                    for (int matrix1_col = 0; matrix1_col < matrix1Cols; matrix1_col++)
+                    {
+                        product[matrix1_row, matrix2_col] +=
+                          matrix1[matrix1_row, matrix1_col] *
+                          matrix2[matrix1_col, matrix2_col];
+                    }
+                }
+            }
+
+            return product;
+        }
+    }
+}
--- CG_Project/Views/AffineTransformationWindow.xaml.cs
+++ CG_Project/Views/AffineTransformationWindow.xaml.cs
@@ -1,43 +1,223 @@
 ﻿using System;
-using System.IO;
 using System.Windows;
-using System.Windows.Input;
-using System.Windows.Media.Imaging;
 using Microsoft.Win32;
-using ColorConverter = CG_Project.Services.ColorsService.ColorConverter;
-using Color = System.Drawing.Color;
-using System.Windows.Controls;
-using System.Windows.Media;
-using System.Windows.Shapes;
 using OxyPlot;
 using OxyPlot.Axes;
 using OxyPlot.Series;
 using OxyPlot.Wpf;
 using OxyPlot.Annotations;
+using System.Threading;
+using CG_Project.Services.AffineTransformation;
+using System.Threading.Tasks;
 
 namespace CG_Project.Views
 {
-    /// <summary>
-    /// Interaction logic for ColorModelsWindow.xaml
-    /// </summary>
     public partial class AffineTransformationWindow : Window
     {
-
         private Window BaseWindow;
+        private AffineTransformator _transformator;
         PlotModel plotModel;
         LineSeries line;
         LineSeries paralellogram;
+        private CancellationTokenSource token;
+        private bool buttonStopWasClicked = false;
 
         public AffineTransformationWindow(Window window)
         {
             InitializeComponent();
             BaseWindow = window;
             plotModel = SetPlot();
+            _transformator = new AffineTransformator();
             buildParalelogram_Click(this, null);
             buildLineBtn_Click(this, null);
         }
 
+        private void PaintFunction(CancellationToken token)
+        {
+            try
+            {
+                #region start
+                int x1 = Int32.Parse(x_1.Dispatcher.Invoke(() => { return x_1.Text; }));
+                int y1 = Int32.Parse(y_1.Dispatcher.Invoke(() => { return y_1.Text; }));
+                int x2 = Int32.Parse(x_1.Dispatcher.Invoke(() => { return x_2.Text; }));
+                int y2 = Int32.Parse(y_1.Dispatcher.Invoke(() => { return y_2.Text; }));
+                int x3 = Int32.Parse(x_1.Dispatcher.Invoke(() => { return x_3.Text; }));
+                int y3 = Int32.Parse(y_1.Dispatcher.Invoke(() => { return y_3.Text; }));
+                int x4 = Int32.Parse(x_1.Dispatcher.Invoke(() => { return x_4.Text; }));
+                int y4 = Int32.Parse(y_1.Dispatcher.Invoke(() => { return y_4.Text; }));
+                #endregion
 
+                //Point chosenVertexAsPoint;
+                //chosenVertexAsPoint.X = x2;
+                //chosenVertexAsPoint.Y = y2;
+                //double[,] points = { { x1, y1, 1 }, { x2, y2, 1 }, { x3, y3, 1 }, { x4, y4, 1 } };
+
+
+                double coofA = Double.Parse(cooficient_A.Dispatcher.Invoke(() => { return cooficient_A.Text; }));
+                double coofB = Double.Parse(cooficient_B.Dispatcher.Invoke(() => { return cooficient_B.Text; }));
+
+
+                double angle = Math.Atan(coofA);
+                // - translate -b
+                // - rotate -angle
+                // - invert y
+                // - rotate angle
+                // - transtlate b
+                AffineTransformation.AffineMatrix translationMatrix =
+                    new AffineTransformation.AffineMatrix(
+                        1,
+                        0,
+                        0,
+                        1,
+                        0,
+                        -coofB);
+
+                AffineTransformation.AffineMatrix rotationMatrix =
+                    new AffineTransformation.AffineMatrix(
+                        Math.Cos(-angle),
+                        -Math.Sin(-angle),
+                        Math.Sin(-angle),
+                        Math.Cos(-angle),
+                        0,
+                        0);
+
+                AffineTransformation.AffineMatrix scalingMatrix =
+                    new AffineTransformation.AffineMatrix(
+                        1,
+                        0,
+                        0,
+                        -1,
+                        0,
+                        0);
+
+                AffineTransformation.AffineMatrix rotationBackMatrix =
+                    new AffineTransformation.AffineMatrix(
+                        Math.Cos(angle),
+                        -Math.Sin(angle),
+                        Math.Sin(angle),
+                        Math.Cos(angle),
+                        0,
+                        0);
+
+                AffineTransformation.AffineMatrix translationBackMatrix =
+                new AffineTransformation.AffineMatrix(
+                    1,
+                    0,
+                    0,
+                    1,
+                    0,
+                    coofB);
+
+                Point origin = new Point(0, 0);
+                Point p1 = new Point(x1, y1);
+                Point p2 = new Point(x2, y2);
+                Point p3 = new Point(x3, y3);
+                Point p4 = new Point(x4, y4);
+
+
+                p1 = AffineTransformation.Transform(p1, origin, translationMatrix);
+                p2 = AffineTransformation.Transform(p2, origin, translationMatrix);
+                p3 = AffineTransformation.Transform(p3, origin, translationMatrix);
+                p4 = AffineTransformation.Transform(p4, origin, translationMatrix);
+
+                p1 = AffineTransformation.Transform(p1, origin, rotationMatrix);
+                p2 = AffineTransformation.Transform(p2, origin, rotationMatrix);
+                p3 = AffineTransformation.Transform(p3, origin, rotationMatrix);
+                p4 = AffineTransformation.Transform(p4, origin, rotationMatrix);
+
+                p1 = AffineTransformation.Transform(p1, origin, scalingMatrix);
+                p2 = AffineTransformation.Transform(p2, origin, scalingMatrix);
+                p3 = AffineTransformation.Transform(p3, origin, scalingMatrix);
+                p4 = AffineTransformation.Transform(p4, origin, scalingMatrix);
+
+                p1 = AffineTransformation.Transform(p1, origin, rotationBackMatrix);
+                p2 = AffineTransformation.Transform(p2, origin, rotationBackMatrix);
+                p3 = AffineTransformation.Transform(p3, origin, rotationBackMatrix);
+                p4 = AffineTransformation.Transform(p4, origin, rotationBackMatrix);
+
+                p1 = AffineTransformation.Transform(p1, origin, translationBackMatrix);
+                p2 = AffineTransformation.Transform(p2, origin, translationBackMatrix);
+                p3 = AffineTransformation.Transform(p3, origin, translationBackMatrix);
+                p4 = AffineTransformation.Transform(p4, origin, translationBackMatrix);
+
+                double[,] newPoints =
+                {
+                    { p1.X, p1.Y },
+                    { p2.X, p2.Y },
+                    { p3.X, p3.Y },
+                    { p4.X, p4.Y }
+                };
+
+                PaintParallelogram(newPoints);
+                //while (true)
+                //{
+                //    points = _transformator.Transform(points, coofA, coofB);
+                //    PaintParallelogram(points);
+                //    token.ThrowIfCancellationRequested();
+                //    Thread.Sleep(paintTimeout);
+                //}
+            }
+            catch (OperationCanceledException e)
+            {
+                return;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Uncorrect input data. Try again \n" + e.Message, "Input Error",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+
+            //try
+            //{
+            //    #region start
+            //    int x1 = Int32.Parse(x_1.Dispatcher.Invoke(() => { return x_1.Text; }));
+            //    int y1 = Int32.Parse(y_1.Dispatcher.Invoke(() => { return y_1.Text; }));
+            //    int x2 = Int32.Parse(x_1.Dispatcher.Invoke(() => { return x_2.Text; }));
+            //    int y2 = Int32.Parse(y_1.Dispatcher.Invoke(() => { return y_2.Text; }));
+            //    int x3 = Int32.Parse(x_1.Dispatcher.Invoke(() => { return x_3.Text; }));
+            //    int y3 = Int32.Parse(y_1.Dispatcher.Invoke(() => { return y_3.Text; }));
+            //    int x4 = Int32.Parse(x_1.Dispatcher.Invoke(() => { return x_4.Text; }));
+            //    int y4 = Int32.Parse(y_1.Dispatcher.Invoke(() => { return y_4.Text; }));
+            //    #endregion
+
+            //    //Point chosenVertexAsPoint;
+            //    //chosenVertexAsPoint.X = x2;
+            //    //chosenVertexAsPoint.Y = y2;
+            //    double[,] points = { { x1, y1, 1 }, { x2, y2, 1 }, { x3, y3, 1 }, { x4, y4, 1 } };
+
+
+            //    int coofA = Int32.Parse(cooficient_A.Dispatcher.Invoke(() => { return cooficient_A.Text; }));
+            //    int coofB = Int32.Parse(cooficient_B.Dispatcher.Invoke(() => { return cooficient_B.Text; }));
+            //    points = _transformator.Transform(points, coofA, coofB);
+            //    PaintParallelogram(points);
+            //    //while (true)
+            //    //{
+            //    //    points = _transformator.Transform(points, coofA, coofB);
+            //    //    PaintParallelogram(points);
+            //    //    token.ThrowIfCancellationRequested();
+            //    //    Thread.Sleep(paintTimeout);
+            //    //}
+            //}
+            //catch (OperationCanceledException e)
+            //{
+            //    return;
+            //}
+            //catch (Exception e)
+            //{
+            //    MessageBox.Show("Uncorrect input data. Try again \n" + e.Message, "Input Error",
+            //        MessageBoxButton.OK, MessageBoxImage.Error);
+            //    return;
+            //}
+        }
+
+        private void playBtn_Click(object sender, RoutedEventArgs e)
+        {
+            buttonStopWasClicked = !buttonStopWasClicked;
+            token = new CancellationTokenSource();
+            Task.Run(() => PaintFunction(token.Token), token.Token);
+        }
 
         private double[,] CalculateFourthParallelogramVertex(double[,] vertexes)
         {
@@ -96,7 +276,7 @@ namespace CG_Project.Views
             {
                 X = Convert.ToDouble(points.X),
                 Y = Convert.ToDouble(points.Y),
-                Text = String.Format($"{name}' ({points.X}; {points.Y})"),
+                Text = String.Format($"{name}' ({Math.Round(points.X, 2)}; {Math.Round(points.Y, 2)})"),
                 FontSize = 18,
                 FontWeight = 2
             });
@@ -224,7 +404,7 @@ namespace CG_Project.Views
             double x2 = (-coofB + Y) / coofA;
 
             line.SeriesGroupName = "Lines";
-            line.Points.Add(new DataPoint(x1, (-Y)));
+            line.Points.Add(new DataPoint(x1, -Y));
             line.Points.Add(new DataPoint(x2, Y));
             plotModel.Series.Add(line);
             line.Color = OxyColor.FromRgb(0, 0, 0);
@@ -246,6 +426,12 @@ namespace CG_Project.Views
         {
             BaseWindow.Visibility = Visibility.Visible;
             this.Visibility = Visibility.Hidden;
+        }
+
+
+        private void resetParalelogram_Click(object sender, RoutedEventArgs e)
+        {
+            buildParalelogram_Click(this, null);
         }
     }
 }
